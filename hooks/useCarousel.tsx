ce6273cb2d49af6f1e40c294bf5d420c01fcb334
Jatesh Parikh <jateshparikh97@gmail.com>
2024-10-07 22:41:44 +0530

    hooks, bottom sheet, image card, and themed safe area view component completed

---
+++ hooks/useCarousel.tsx
@@ -0,0 +1,19 @@
+export function useCarousel(): { title: string; image: string }[] {
+  return [
+    {
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/grtS8AsZRGyLm3VJ2SHS7g",
+      title: "In the Sunset",
+    },
+    {
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/xPCpOvQYSVqIjuo2mjBPmg",
+      title: "Close to me",
+    },
+    {
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/CNhPzxJ1RO2hO8RXF4dang",
+      title: "In here",
+    },
+  ];
+}
